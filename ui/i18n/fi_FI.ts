917983f6b5a4c2db149611f75ff222799d830fc1
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 21:17:38 -0500

    #24 Estimated time for Finnish

--- ui/i18n/fi_FI.ts
+++ ui/i18n/fi_FI.ts
@@ -1172,8 +1172,8 @@ Allow to open and proceed to the website? You can always update your choice in S
         <translation>s</translation>
     </message>
     <message id="loading-view-estimate-time">
-        <source>Estimate time: %s</source>
-        <translation>Arvioitu kesto: %s</translation>
+        <source>Estimated time: %s</source>
+        <translation>Arvioitu aika: %s</translation>
     </message>
     <message id="general-fee-rate">
         <source>%1 Transaction fee rate</source>
