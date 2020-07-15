b7ccf246b97ade75dd3931db95a32cedb441a42f
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 19:53:08 -0500

    Update rs_RS.ts

--- ui/i18n/rs_RS.ts
+++ ui/i18n/rs_RS.ts
@@ -346,8 +346,8 @@
         <translation type="unfinished">Примени</translation>
     </message>
     <message id="logo-description">
-        <source>Scalable confidential cryptocurrency</source>
-        <translation type="unfinished">Scalable confidential cryptocurrency</translation>
+        <source>Confidential, fast, easy to use</source>
+        <translation>Поверљиво, брзо, лако за коришћење</translation>
     </message>
     <message id="change-pwd-old-empty">
         <source>Please, enter old password</source>
