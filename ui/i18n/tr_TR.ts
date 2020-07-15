7e5710a7657a84938d0d4700e280423a122b890f
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 22:09:36 -0500

    #24 Estimated time for Turkish

--- ui/i18n/tr_TR.ts
+++ ui/i18n/tr_TR.ts
@@ -1173,7 +1173,7 @@ Allow to open and proceed to the website? You can always update your choice in S
     </message>
     <message id="loading-view-estimate-time">
         <source>Estimated time: %s</source>
-        <translation>Tahmini süre: %s</translation>
+        <translation>Tahmini Süre: %s</translation>
     </message>
     <message id="general-fee-rate">
         <source>%1 Transaction fee rate</source>
