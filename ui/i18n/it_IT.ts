ab9cdfdb8e841ac227c83f923ed680d7fde64fcc
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 19:44:38 -0500

    Update it_IT.ts

--- ui/i18n/it_IT.ts
+++ ui/i18n/it_IT.ts
@@ -346,8 +346,8 @@
         <translation type="unfinished">Applicare</translation>
     </message>
     <message id="logo-description">
-        <source>Scalable confidential cryptocurrency</source>
-        <translation>Criptovaluta confidenziale scalabile</translation>
+        <source>Confidential, fast, easy to use</source>
+        <translation>Riservato, veloce, facile da usare</translation>
     </message>
     <message id="change-pwd-old-empty">
         <source>Please, enter old password</source>
