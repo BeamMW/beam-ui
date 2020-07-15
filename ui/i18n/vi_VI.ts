fcb7e8b59e330301779d851ec00771c86b2ea012
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 22:11:04 -0500

    #24 Estimation time for Vietnamese

--- ui/i18n/vi_VI.ts
+++ ui/i18n/vi_VI.ts
@@ -1172,7 +1172,7 @@ Allow to open and proceed to the website? You can always update your choice in S
         <translation type="unfinished">sec</translation>
     </message>
     <message id="loading-view-estimate-time">
-        <source>Estimate time: %s</source>
+        <source>Estimated time: %s</source>
         <translation>Thời gian dự kiến: %s</translation>
     </message>
     <message id="general-fee-rate">
