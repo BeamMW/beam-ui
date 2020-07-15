22a18fce97bea30b3782aa4a2f99c7ae836c6485
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 21:00:01 -0500

    #24 Estimated Time Line 1176 for Belarusian

--- ui/i18n/be_BY.ts
+++ ui/i18n/be_BY.ts
@@ -1172,7 +1172,7 @@ Allow to open and proceed to the website? You can always update your choice in S
     </message>
     <message id="loading-view-estimate-time">
         <source>Estimated time: %s</source>
-        <translation>Разліковы час:</translation>
+        <translation>Разліковы час: </translation>
     </message>
     <message id="general-fee-rate">
         <source>%1 Transaction fee rate</source>
