d9e4800931b9cff3388899822598f5b3c05cf7b0
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 21:48:40 -0500

    #24 Estimated time for Serbian

--- ui/i18n/rs_RS.ts
+++ ui/i18n/rs_RS.ts
@@ -1172,8 +1172,8 @@ Allow to open and proceed to the website? You can always update your choice in S
         <translation type="unfinished">sec</translation>
     </message>
     <message id="loading-view-estimate-time">
-        <source>Estimate time: %s</source>
-        <translation>Предвиђено време: %s</translation>
+        <source>Estimated time: %s</source>
+        <translation>Процењено време: %s</translation>
     </message>
     <message id="general-fee-rate">
         <source>%1 Transaction fee rate</source>
