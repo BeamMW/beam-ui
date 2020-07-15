a0234fd6eba9df3f3e7bbf741d1d8bcc1cbe1710
SomethingGettingWrong <36722911+SomethingGettingWrong@users.noreply.github.com>
2020-07-14 20:02:32 -0500

    Update tr_TR.ts

--- ui/i18n/tr_TR.ts
+++ ui/i18n/tr_TR.ts
@@ -346,8 +346,8 @@
         <translation type="unfinished">Uygula</translation>
     </message>
     <message id="logo-description">
-        <source>Scalable confidential cryptocurrency</source>
-        <translation>Ölçeklenebilir gizlilik odaklı kriptopara</translation>
+        <source>Confidential, fast, easy to use</source>
+        <translation>Gizli, hızlı, kullanımı kolay</translation>
     </message>
     <message id="change-pwd-old-empty">
         <source>Please, enter old password</source>
