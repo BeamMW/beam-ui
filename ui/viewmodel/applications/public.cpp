f101b768c3fd2af838a5a0d78bd54f2fc32a96c5
anatol <sevostyanaa@gmail.com>
2020-11-05 19:59:53 +0300

    fixed build

--- ui/viewmodel/applications/public.cpp
+++ ui/viewmodel/applications/public.cpp
@@ -1,7 +1,26 @@
+
+#if defined(_MSC_VER)
+#pragma warning (push)
+#pragma warning (disable: 4127)
+#endif
 #include <QQmlEngine>
+#if defined(_MSC_VER)
+#pragma warning (pop)
+#endif
+
 #include "public.h"
 #include "apps_view.h"
+
+#if defined(_MSC_VER)
+#pragma warning (push)
+#pragma warning (disable: 4251)
+#endif
 #include "webapi_beam.h"
+#if defined(_MSC_VER)
+#pragma warning (pop)
+#endif
+
+
 
 namespace beamui::applications
 {
