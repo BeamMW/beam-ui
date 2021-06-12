5a497bacf94d554b2286fd5900c7775ec2a07bcc
chapati <chapati.th@gmail.com>
2021-06-12 11:00:07 +0300

    apps restored

--- ui/model/settings.cpp
+++ ui/model/settings.cpp
@@ -675,7 +675,7 @@ QString WalletSettings::getAppsUrl() const
     #elif defined(BEAM_MAINNET)
     return "https://apps.beam.mw/appslist.json";
     #else
-    return "http://3.136.182.25:80/app/appslist.json";
+    return "http://3.19.141.112/app/appslist.json";
     #endif
 }
 
