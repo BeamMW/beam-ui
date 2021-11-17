5534dcd38dc8661615f8d4b5183f3a9a1cff33fc
chapati <chapati.th@gmail.com>
2021-11-17 22:02:19 +0300

    #869 - mutex disabled

--- ui/model/app_model.cpp
+++ ui/model/app_model.cpp
@@ -93,11 +93,11 @@ AppModel::AppModel(WalletSettings& settings)
     const auto dbFilePath = m_settings.getAppDataPath();
     auto mutexName = QCryptographicHash::hash(dbFilePath.c_str(), QCryptographicHash::Md5).toHex();
     /*m_dbGuard = std::make_unique<boost::interprocess::named_mutex>(boost::interprocess::open_or_create, mutexName);
-    if (m_dbGuard->try_lock())
+    if (m_dbGuard->try_lock())*/
     {
         m_isOnlyOneInstanceStarted = true;
         m_nodeModel.start();
-    }*/
+    }
 }
 
 AppModel::~AppModel()
