11ba2495c873efa928429fce3cea6a78cfc04286
glaush <56369021+glaush@users.noreply.github.com>
2022-01-31 22:36:25 +0300

    Update en_US.ts: added alignment to some lines.

--- ui/i18n/en_US.ts
+++ ui/i18n/en_US.ts
@@ -2698,10 +2698,10 @@ Update your settings and try again.</source>
         <source>Whitepaper</source>
         <translation type="unfinished"></translation>
     </message>
-	<message id="tx-details-date-label">
-		<source>Date</source>
-		<translation type="unfinished"></translation>
-	</message>
+    <message id="tx-details-date-label">
+	<source>Date</source>
+	<translation type="unfinished"></translation>
+    </message>
     <message id="tx-details-sending-addr-label">
         <source>Sending address</source>
         <translation type="unfinished"></translation>
