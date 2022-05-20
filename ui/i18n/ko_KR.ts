fd5fea3a95a39e9be2a2f4fb1e3b2d05f9833b08
sergey.zavarza <zavarza@beam-mw.com>
2022-05-20 17:44:48 +0200

    #955 Incorrect display "Payment proof verification" window

--- ui/i18n/ko_KR.ts
+++ ui/i18n/ko_KR.ts
@@ -358,10 +358,6 @@
         <source>Copy details</source>
         <translation>세부사항 복사하기</translation>
     </message>
-    <message id="payment-info-copy-code-button">
-        <source>Copy code</source>
-        <translation>코드 복사하기</translation>
-    </message>
     <message id="loading-try-again-button">
         <source>Try again</source>
         <translation>다시 시도하기</translation>
