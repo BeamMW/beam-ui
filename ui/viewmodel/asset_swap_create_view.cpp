ddfb216cb49ed4a4e15b093f5559a8f26c4381e8
sergey.zavarza <zavarza@beam-mw.com>
2022-09-20 13:35:21 +0300

    #988 Assets swap: change error comment under the send amount field when the Beam balance is 0

--- ui/viewmodel/asset_swap_create_view.cpp
+++ ui/viewmodel/asset_swap_create_view.cpp
@@ -95,6 +95,7 @@ void AssetSwapCreateViewModel::onCoinsSelected(const beam::wallet::CoinsSelectio
     }
 
     _isEnoughtToSend = selectionRes.m_isEnought;
+    _isFeeEnoughtToSend = !_isEnoughtToSend && selectionRes.m_selectedSumBeam != 0;
     _maxAmountToSendGrothes = selectionRes.get_NettoValue();
     emit canCreateChanged();
 }
@@ -282,6 +283,11 @@ bool AssetSwapCreateViewModel::getCanCreate() const
 bool AssetSwapCreateViewModel::getIsEnough() const
 {
     return _amountToSendGrothes == 0 || _isEnoughtToSend;
+}
+
+bool AssetSwapCreateViewModel::getIsFeeEnough() const
+{
+    return _amountToSendGrothes == 0 || _isFeeEnoughtToSend;
 }
 
 QString AssetSwapCreateViewModel::getMaxSendAmount() const
