45e3666252bf5a482d7084272f95a31a554feca3
anatol <sevostyanaa@gmail.com>
2023-12-06 20:57:14 +0200

    test notarytool

--- notarize.sh
+++ notarize.sh
@@ -4,33 +4,8 @@
 # Notarize the dmg
 notarize_dmg() {(
   echo "Uploading $BEAM_WALLET_UI_IN to notarization service"
-  uploadRes=$(xcrun altool --notarize-app --primary-bundle-id "com.mw.beam.beamwallet" --username "$MACOS_NOTARIZE_USER" --password "$MACOS_NOTARIZE_PASS" --asc-provider "$MACOS_NOTARIZE_PROVIDER" --file "$BEAM_WALLET_UI_IN" 2>&1)
-  echo "Result: $uploadRes"
-  uuid=$(echo "$uploadRes" | grep 'RequestUUID' | awk '{ print $3 }')
-  if [ "$uuid" = "" ]; then
-    echo "Uploading to notarization service error"
-    exit 1
-  else
-    echo "Successfully uploaded to notarization service, polling for result: $uuid"
-  fi
-  sleep 15
-  while :
-  do
-    fullstatus=$(xcrun altool --notarization-info "$uuid" --username "$MACOS_NOTARIZE_USER" --password "$MACOS_NOTARIZE_PASS" --asc-provider "$MACOS_NOTARIZE_PROVIDER" --verbose 2>&1)
-    status=$(echo "$fullstatus" | grep 'Status\:' | awk '{ print $2 }')
-    if [ "$status" = "success" ]; then
-      xcrun stapler staple "$BEAM_WALLET_UI_IN"
-      echo "Notarization success"
-      return
-    elif [ "$status" = "in" ]; then
-      echo "Notarization still in progress, sleeping for 15 seconds and trying again"
-      sleep 15
-    else
-      echo "Notarization failed fullstatus below"
-      echo "$fullstatus"
-      exit 1
-    fi
-  done
+  xcrun notarytool submit "$BEAM_WALLET_UI_IN" --apple-id "$MACOS_NOTARIZE_USER" --team-id "$MACOS_NOTARIZE_PROVIDER" --password "$MACOS_NOTARIZE_PASS" --verbose --wait
+  xcrun stapler staple "$BEAM_WALLET_UI_IN"
 )}
 
 notarize_dmg
